9037e079e2011b07860f117167b13f1edd17df6f
HuynhKietFPTedu <kiethttps32354@fpt.edu.vn>
2024-08-02 23:46:43 +0700

    Update index.html

--- CrawlData/obj/Debug/net8.0/CrawlData.AssemblyInfo.cs
+++ CrawlData/obj/Debug/net8.0/CrawlData.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrawlData")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e6bcd28d7440d7faf1ebbaf8582e92e0af32a5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab8df386589961177172528f83a715ad94383481")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrawlData")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrawlData")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
